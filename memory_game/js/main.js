161e218168639009f84705ecde69a8d6c13b0a0c
Brian <briankrabec@gmail.com>
2020-02-09 03:18:00 -0600

    function update to created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,22 @@
-console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	let cardOne = true;
-
-	cardOne = cardTwo === true ? alert("You found a match!") : alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+	}
 }
+
+function flipCard(cardId) {
+	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+flipCard(0);
+flipCard(2);
