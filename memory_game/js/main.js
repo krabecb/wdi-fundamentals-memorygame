d91d27c8a9c214c84e0ca4550d6fd09bfe568b39
Brian <briankrabec@gmail.com>
2020-02-09 16:35:32 -0600

    final update

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,23 +23,34 @@ cardImage: 'images/king-of-diamonds.png'
 
 const cardsInPlay = [];
 
-function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");
-	} else {
-  		console.log("Sorry, try again.");
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+    	let cardElement = document.createElement('img');
+    	cardElement.setAttribute('src', 'images/back.png');
+    	cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener('click', flipCard);
+    	document.getElementById('game-board').appendChild(cardElement);
 	}
 }
 
-function flipCard(cardId) {
-	
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		alert("You found a match!");
+	} else {
+  		alert("Sorry, try again.");
+	}
+}
+
+function flipCard() {
+	let cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+
+createBoard();
